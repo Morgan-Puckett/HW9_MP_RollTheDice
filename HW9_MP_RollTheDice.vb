1305e148f374c52fff0a145cc2de8204a99bfb49
Morgan Puckett <morganpuckett02@gmail.com>
2022-02-15 08:05:03 -0700

    Add project files.

---
+++ HW9_MP_RollTheDice.vb
@@ -0,0 +1,45 @@
+﻿
+Option Strict On
+Option Explicit On
+Module HW9_MP_RollTheDice
+
+    Sub main()
+        Dim rndNumbers(7) As Integer
+        Dim temp As String
+        For i = 1 To 1000
+            rndNumbers(GetRandomInteger()) += 1
+        Next
+
+        For i = 0 To 7
+            temp = CStr(i) & "  |"
+            temp = temp.PadLeft(6)
+            Console.Write(temp)
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine(StrDup(48, "="))
+
+        For i = 0 To 7
+            temp = CStr(rndNumbers(i)) & " |"
+            temp = temp.PadLeft(6)
+            Console.Write(temp)
+        Next
+
+        Console.WriteLine()
+
+        Console.WriteLine(StrDup(48, "="))
+
+        Console.ReadLine()
+    End Sub
+
+    Function GetRandomInteger() As Integer
+
+        Randomize()
+        'generates value between 1-6
+        Dim value As Integer = CInt(Int((6 * Rnd()) + 1))
+
+
+        Return value
+    End Function
+
+End Module
