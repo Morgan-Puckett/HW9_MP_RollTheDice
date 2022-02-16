2a795fb3da07a140c00e43a3b2ac5bd503c0a096
Morgan Puckett <morganpuckett02@gmail.com>
2022-02-15 20:15:09 -0700

    final touches

--- HW9_MP_RollTheDice.vb
+++ HW9_MP_RollTheDice.vb
@@ -1,44 +1,57 @@
-﻿
+﻿'Morgan Puckett
+'RCET0265
+'Spring 2022
+'Roll the Dice
+
 Option Strict On
 Option Explicit On
 Module HW9_MP_RollTheDice
-
+    'this varible is used in a function outside of the main and therefor must be kept as a field varible
+    Dim arrayLength As Integer = 7
     Sub main()
-        Dim rndNumbers(7) As Integer
+        'these varibles allow for easy access to edit the front panel of this program 
+        Dim rndNumbers(arrayLength) As Integer
         Dim temp As String
-        For i = 1 To 1000
+        Dim separator As String = "="
+        Dim lineSeparator As String = "|"
+        Dim padLength As Integer = 6
+        Dim lineLength = padLength * ((UBound(rndNumbers)) + 1)
+
+        'runs 10000 random number generators
+        For i = 1 To 10000
             rndNumbers(GetRandomInteger()) += 1
         Next
 
-        For i = 0 To 7
-            temp = CStr(i) & "  |"
-            temp = temp.PadLeft(6)
+        For i = LBound(rndNumbers) To (UBound(rndNumbers))
+            temp = CStr(i) & lineSeparator
+            temp = temp.PadLeft(padLength)
             Console.Write(temp)
         Next
         Console.WriteLine()
+        'separates the first row from the second 
+        Console.WriteLine(StrDup(lineLength, separator))
 
-        Console.WriteLine(StrDup(48, "="))
-
-        For i = 0 To 7
-            temp = CStr(rndNumbers(i)) & " |"
-            temp = temp.PadLeft(6)
+        For i = LBound(rndNumbers) To (UBound(rndNumbers))
+            temp = CStr(rndNumbers(i)) & lineSeparator
+            temp = temp.PadLeft(padLength)
             Console.Write(temp)
         Next
 
         Console.WriteLine()
 
-        Console.WriteLine(StrDup(48, "="))
+        Console.WriteLine(StrDup(lineLength, separator))
 
         Console.ReadLine()
     End Sub
-
+    'this function allows a random number to be chosen between the upper and lower bounds of the array length
     Function GetRandomInteger() As Integer
+        Dim value As Integer
 
+        Dim temp As Single
         Randomize()
-        'generates value between 1-6
-        Dim value As Integer = CInt(Int((6 * Rnd()) + 1))
-
-
+        temp = Rnd() * arrayLength
+        value = CInt(System.Math.Floor(CDbl(temp)))
+        'generates value between 1-chosen array length
         Return value
     End Function
 
